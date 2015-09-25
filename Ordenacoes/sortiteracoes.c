6b1efd33d2e16bd85c7bf069dc90f35145fac565
wagjub <danielcostavalerio@hotmail.com>
2015-09-25 18:22:16 -0300

    funcoes implementadas.... arrumar o contador de iterações e colocar o tempo

--- Ordenacoes/sortiteracoes.c
+++ Ordenacoes/sortiteracoes.c
@@ -12,28 +12,28 @@ int* BubbleSort(int* v, int size)
 	int* v2; 
 	int aux;
 	Bool validator = false;
-	
+
 	count = 0;
 	v2 = (int*) malloc(sizeof(int)*size);
 	memmove(v2, v, sizeof(int)*size);
-	
-		while (!validator)
+
+	while (!validator)
+	{
+		validator = true;
+
+		for (int i=1; i<size; i++)
 		{
-			validator = true;
-		
-			for (int i=1; i<size; i++)
-			{
 			count++;
-				if (v2[i] < v2[i-1])
-				{
-					aux = v2[i];
-					v2[i] = v2[i-1];
-					v2[i-1] = aux; 
-					validator = false;
-				}
+			if (v2[i] < v2[i-1])
+			{
+				aux = v2[i];
+				v2[i] = v2[i-1];
+				v2[i-1] = aux; 
+				validator = false;
 			}
+		}
 		size--;
-		} 
+	} 
 	return(v2);
 }
 
@@ -42,11 +42,11 @@ int* SelectionSort(int* v, int size)
 	int* v2; 
 	int aux, max;
 	Bool validator=false;
-	
+
 	count = 0;
 	v2 = (int*) malloc(sizeof(int)*size);
 	memmove(v2, v, sizeof(int)*size);
-	
+
 	while (!validator)
 	{
 		max = 0;
@@ -56,14 +56,14 @@ int* SelectionSort(int* v, int size)
 			{
 				max = i;
 			}	
-		count++;
+			count++;
 		} 
 		aux = v2[size-1];
 		v2[size-1] = v2[max];
 		v2[max] = aux;
 		size--;
 
-		if (max == 0) // verifica se o vetor está ordenado quando não foi feita nenhuma alteração
+		if (max == 0) // verifica se o v2 está ordenado quando não foi feita nenhuma alteração
 		{
 			for (int j=1; j<size; j++)
 			{
@@ -75,10 +75,79 @@ int* SelectionSort(int* v, int size)
 			}
 		}
 	}
-return(v2);
+	return(v2);
 }
 
-int* InsertionSort();
+int* InsertionSort(int* v2, int size)
+{
+	int i, j, aux;
+	int* v; 
+
+	v = (int*) malloc(sizeof(int)*size);
+	memmove(v, v2, sizeof(int)*size);
+
+	count = 0;
+
+	for (i = 1; i < size; i++) {
+		aux = v[i];
+
+		j = i;
+
+		while ((aux < v[j - 1]) && (j > 0))
+		{
+			v[j] = v[j - 1];
+			j--;
+			count++;
+		}
+
+		v[j] = aux;
+	}
+	return(v);
+}
+
+int* ShellSort(int* v, int size)
+{
+	int k , i, j;
+
+	int* v2; 
+	int aux;
+
+	count = 0;
+
+	v2 = (int*) malloc(sizeof(int)*size);
+	memmove(v2, v, sizeof(int)*size);
+
+	for(k = 1; k < size; k = 3*k+1) /* só para calcular o k */
+	{
+		k = size/2;
+	}
+
+
+	while (k > 0) {
+
+		for (i = k; i < size; i++) 
+		{
+			aux = v2[i];
+			j = i;
+
+			/* efetua comparações entre elementos com distância k: */
+
+			while ((v2[j - k] > aux) && (j >= k))
+			{
+				v2[j] = v2[j - k];
+				j = j-k;
+			}
+
+			v2[j] = aux;
+		}
+
+		k = (k-1)/3; /* atualiza o valor de k. */
+
+		k = k/2;
+
+	}
+return(v2);
+}
 
 int* QuickSort();
 
@@ -89,7 +158,7 @@ int main()
 	int v1[]={7,13,5,8,9,2,3};
 	int size = 7;
 	int* vOrd;
-	
+
 	vOrd = BubbleSort(v1, size);
 	printf("Bubble Sort\nVetor Ordenado:\n");	
 	for (int i=0; i<size; i++)
@@ -97,7 +166,7 @@ int main()
 		printf("%d ", vOrd[i]);
 	}
 	printf("\nNúmero de iterações: %d\n", count);
-		free(vOrd);
+	free(vOrd);
 
 	vOrd = SelectionSort(v1, size);
 	printf("Selection Sort\nVetor Ordenado:\n");	
@@ -106,6 +175,26 @@ int main()
 		printf("%d ", vOrd[i]);
 	}
 	printf("\nNúmero de iterações: %d\n", count);
+	free(vOrd);
+
+
+	vOrd = InsertionSort(v1, size);
+	printf("Instertion Sort\nVetor Ordenado:\n");	
+	for (int i=0; i<size; i++)
+	{
+		printf("%d ", vOrd[i]);
+	}
+	printf("\nNúmero de iterações: %d\n", count);
+	free(vOrd);
+
+	vOrd = ShellSort(v1, size);
+	printf("Shell Sort\nVetor Ordenado:\n");	
+	for (int i=0; i<size; i++)
+	{
+		printf("%d ", vOrd[i]);
+	}
+	printf("\nNúmero de iterações: %d\n", count);
+	free(vOrd);
 
 	return(0);
 }
