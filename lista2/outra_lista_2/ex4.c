9f0495f70332bfd73e422c0a202c81f9c9dc350d
WaGjUb <danielvalerio@alunos.utfpr.edu.br>
2015-10-02 17:25:06 -0300

    quicksort funcionando melhor do que nunca rapazgit status

--- lista2/outra_lista_2/ex4.c
+++ lista2/outra_lista_2/ex4.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
-#define TAM 7
+#define TAM 7 
 
 int Partition(int* v,int ini, int fim)
 {
-	int pivo = vetor[ini], aux; //posição inicial do pivo, primeiro elemento do vetor
+	int pivo = v[ini], aux, i; //posição inicial do pivo, primeiro elemento do v
 
-	ini++;
-	while (ini < fim)
+	i = ini;
+	
+		while (ini < fim)
 	{ 
-		while((vetor[ini] <= pivo) && (ini < fim))
+		while((v[ini] < pivo) && (ini < fim))
 		{
 			ini++;
 		}
-		while((vetor[fim] > pivo) && (ini < fim))
+		while((v[fim] > pivo) && (ini < fim))
 		{
 			fim--;
 		}
 	
 		if (ini < fim)
 		{
-			aux = vetor[ini];
-			vetor[ini] = vetor[fim];
-			vetor[fim] = aux;
+			aux = v[ini];
+			v[ini] = v[fim];
+			v[fim] = aux;
 		}
 
 		else
@@ -29,31 +30,46 @@ int Partition(int* v,int ini, int fim)
 			break;
 		}
 	}
-	
-return(ini); //meio do vetor	
+//		aux = v[i];//tira o pivo do local inicial e joga no meio
+//		v[i] = v[fim];
+//		v[fim] = aux;	
+return(ini); //meio do vetor
 	
 }
 
-int QuickSort(int* v, int tamanho)
+int QuickSort(int* v, int ini, int fim)
 {
-	if (tamanho == 0) //condição de parada
+	if (ini >= fim) //condição de parada
 	{
 		return(0); 
 	}
 	
-	int meio;
+	int meio=ini;
 	
-	meio = Partition(v,0, tamanho);
-	Quicksort(v+1, tamanho-1);
-
+	meio = Partition(v,ini, fim);
+	QuickSort(v,meio+1, fim);
+	QuickSort(v, ini, meio-1);
+	
 
 	return(1);
+}
+
+void ImprimeVetor(int* v, int tam)
+{
+	int i;
+	
+	for (i=0; i<tam; i++)
+	{
+		printf("%d\n", v[i]); 
+	}
 }
 
 int main(){
 
 int v[TAM] = {7, 13, 5, 8, 9, 2, 3};	
 
+QuickSort(v, 0, TAM-1);
+ImprimeVetor(v, TAM);
 	
 return(0);
 }
